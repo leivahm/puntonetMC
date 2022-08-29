860ead1029756c2cbb1f4a5bb3840c2c4899394f
leivahm <leivahmarcelo@gmail.com>
2022-08-29 20:38:19 -0300

    repaso de las clases y conceptos anteriores

---
+++ repaso_conceptos/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// Ejemplo 1
+int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+int todosLosNumeros = numeros.Count();
+Console.WriteLine(todosLosNumeros);
+
+int numerosImpares = numeros.Count( n => n % 2 == 1); //Esto resume las líneas 9 a 13
+
+numerosImpares = numeros.Count(  // ESTO
+    n =>                         // HACE
+    {                            // LO MISMO
+        return n % 2 == 1;       // QUE LA 
+    });                          // LÍNEA 7
+Console.WriteLine(numerosImpares);
