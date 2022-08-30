d9701ac3c15466c33d768341ddeed0323f1c956b
leivahm <leivahmarcelo@gmail.com>
2022-08-30 00:55:02 -0300

    clase 21 desde el principio, falta contenido clases: Empleado y Desempleado

---
+++ clase21Mio/ConsoleApp1/Modelo/Jubilado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Modelo
+{
+    public class Jubilado : Persona
+    {
+        public int AniosDeAporte { get; set; } 
+        public char Categoria { get; set; }
+    }
+}
---
+++ clase21Mio/ConsoleApp1/Repositorio/RepositorioDePersonas.cs
@@ -0,0 +1,52 @@
+﻿using ConsoleApp1.Modelo;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1.Repositorio
+{
+    public class RepositorioDePersonas
+    {
+        //Usamos un diccionario para guardar las personas.
+        public Dictionary<string, Persona> Personas { get; set; }
+
+        public RepositorioDePersonas()
+        {
+            Personas = new Dictionary<string, Persona>();
+        }
+
+        public void Insertar(Persona persona)
+        {
+            var numeroDocumento = persona.NumeroDeDocumento;
+            var personaExiste = Personas.ContainsKey(numeroDocumento);
+            if(!personaExiste)
+            {
+                Personas[numeroDocumento] = persona;
+            }
+        }
+
+        public void Eliminar(string numeroDocumento)
+        {
+            Personas[numeroDocumento] = null;
+        }
+
+        public void Actualizar(Persona persona)
+        {
+            var personaAActualizar = Personas[persona.NumeroDeDocumento];
+            if(personaAActualizar != null)
+            {
+                personaAActualizar.Nombre = persona.Nombre;
+                personaAActualizar.Apellido = persona.Apellido;
+                personaAActualizar.FechaNacimiento = persona.FechaNacimiento;
+            }
+        }
+
+        public bool Existe(string numeroDeDocumento)
+        {
+            return Personas.ContainsKey(numeroDeDocumento);
+        }
+
+        public bool Existe(Persona persona)
+        {
+            return Existe(persona.NumeroDeDocumento);
+        }
+    }
+}
